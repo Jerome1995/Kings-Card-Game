32f50c9aa50d8a9a9db5c0f5c82e43df7eb34540
jerome1995 <1995jerome@gmail.com>
2017-02-14 14:53:37 +0000

    In this commit the classes 'Players' and 'Card' were created
    The 'Card' class was pretty straight forward with its variables for cardName and cardRule. It consisted of a no arguement constructor and a multi arguement constructor as well as getters and setters. That is all this class consisted of.
    
    The 'Players' class was more complex. Its variables were a string 'playerName' and a List<String> called 'playerList'. Again this class contained an no argurment and multi argurement constructor. It had functions as follows:
         addPlayer
         getPlayer
         nextPlayer
         removePlayer
         changePlayerName
         nameExists
    'nameExits' and 'getPlayer' are new functions that were not included in the UML diagram. Once I began coding I saw that there was a need for these functions.
    All functions work as they are called, either using loops tom transverse through the list or using functions inheriated from the list.
    The main different between getPlayer and nextPlayer is that getCard will return the first vlaue of the list. While nextCard will read a value given by the user and then output the next value in the list. If it reaches the end of the list it will go back to the start of the list again.

---
+++ Kings Card Game/Kings Card Game/Card.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kings_Card_Game
+{
+    public class Card
+    {
+        private string cardName;
+        private string cardRule;
+
+        public Card()
+        {
+            cardName = "";
+            cardRule = "";
+        }
+
+        public Card(string name, string rule)
+        {
+            this.cardName = name;
+            this.cardRule = rule;
+        }
+
+        public void setCardName(string name)
+        {
+            this.cardName = name;
+        }
+
+        public void setCardRule(string rule)
+        {
+            this.cardRule = rule;
+        }
+
+        public string getCardName()
+        {
+            return cardName;
+        }
+
+        public string getCardRule()
+        {
+            return cardRule;
+        }
+    }
+}
---
+++ Kings Card Game/Kings Card Game/Players.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kings_Card_Game
+{
+    public class Players
+    {
+        private string playerName;
+        private List<String> playerList;
+        private int i = 0;
+
+        public Players()
+        {
+            playerName = "";
+            playerList = new List<string>();
+        }
+
+        public Players(string name)
+        {
+            this.playerName = name;
+            playerList.Add(playerName);
+        }
+
+        public void addPlayer(string name)
+        {
+            this.playerName = name;
+            playerList.Add(playerName);
+        }
+
+        public string getPlayer()
+        {
+            playerName = playerList[0];
+            return playerName;
+        }
+        public string nextPlayer(string previouisName)
+        {
+            i = 0;
+            while (i <= playerList.Count)
+            {
+                if (playerList[i].Equals(previouisName))
+                {
+                    if (i == playerList.Count)
+                    {
+                        return playerList[0];
+                    }
+                    return playerList[i + 1];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return "Unknown";
+        }
+
+        public Boolean removePlayer(string name)
+        {
+            return playerList.Remove(name);
+        }
+
+        public Boolean changePlayerName(string oldName,string newName)
+        {
+            i = 0;
+            while (i <= playerList.Count)
+            {
+                if (playerList[i].Equals(oldName))
+                {
+                    playerList.RemoveAt(i);
+                    playerList.Insert(i, newName);
+                    return true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return false;
+        }
+
+        public Boolean nameExists(string name)
+        {
+            Predicate<String> checkName = (String p) => { return p == name; };
+            return playerList.Exists(checkName);
+        }
+    }
+}
